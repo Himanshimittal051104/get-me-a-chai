8828bd1129a2ba0bb85860fa86f3af50b38083d7
himanshimittal <mittalh363@gmail.com>
2024-09-19 18:24:47 +0530

    Updated payment Functionality

--- pages/api/create-order.js
+++ pages/api/create-order.js
@@ -1,14 +1,12 @@
-import { initiate } from '../../path/to/your/initiate/actions/page'; // Path to your server-side logic
+import { initiate } from "@/actions/useractions";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         try {
             const { amount, to_username, paymentform } = req.body;
 
-            // Call the initiate function to create a Razorpay order
             const order = await initiate(amount, to_username, paymentform);
 
-            // Respond with the order ID
             res.status(200).json({ order_id: order.id });
         } catch (error) {
             console.error('Error creating order:', error);
