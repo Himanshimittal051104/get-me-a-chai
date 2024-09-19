c9372afb8a102440aceedaebfb43a3133054c2fe
himanshimittal <mittalh363@gmail.com>
2024-09-19 16:36:57 +0530

    Updated the Pay Butoon Functionality

--- actions/useractions.js
+++ actions/useractions.js
@@ -2,4 +2,36 @@
 import Razorpay from "razorpay"
 import Payment from "@/models/Payment"
 import connectDB from "@/db/connectDb"
+import User from "@/models/User"
 
+export const initiate = async (amount,currency,to_username, paymentform) => {
+    try{
+        await connectDB();
+        var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET'})
+    
+        const options={
+            amount:Number.parseInt(amount),
+            currency:"INR",
+            receipt:`receipt#${new Date().getTime()}`,
+            notes:{
+                to_user: to_username,
+                name: paymentform.name,
+                message: paymentform.message
+            }
+        }
+        const order=await instance.orders.create(options);
+    
+        await Payment.create({
+            oid:order.id ,
+            amount:amount,
+            to_username:to_username,
+            name:paymentform.name,
+            message:paymentform.message,
+        })
+        return order;
+        
+    }catch(error){
+        console.error("Error initiating payment:", error);
+        throw new Error("Payment initiation failed.");
+    }
+}
--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,80 +1,101 @@
 "use client"
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
 
-
 const Username = () => {
-    const {username}=useParams();
-    const [inputvalue, setInputvalue] = useState('');
+    const { username } = useParams();
+    const [inputvalue, setInputValue] = useState('');
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleName=(event)=>{
-        const value=event.target.value;
-        setName(value);
-    };
-    const handleMessage=(event)=>{
-        const value=event.target.value;
-        setMessage(value);
-    };
-    const handleInput=(event)=>{
-        const value=event.target.value;
-        setInputvalue(value);
-    };
-    const handlePay10=()=>{
-        setInputvalue(10);
-    };
-    const handlePay20=()=>{
-        setInputvalue(20);
-    };
-    const handlePay30=()=>{
-        setInputvalue(30);
-    };
+    const handleName = (event) => setName(event.target.value);
+    const handleMessage = (event) => setMessage(event.target.value);
+    const handleInput = (event) => setInputValue(event.target.value);
 
-  return (
-    <div>
-    <div className='cover'> 
-        <Image src="./landscape1.jpg" alt="image of backgroung"  className=' h-72 w-[100%] object-cover ' />
-    </div>
-    <div className='h-1 bg-blue-950'></div>
-    <div className='text-white text-center relative'>
-        <div className='w-20 h-20 rounded-full bg-black border-2 overflow-hidden border-black mx-auto absolute left-[47vw] -top-11'>
-            <Image src="./lion.jpeg" alt="image of lion" /></div>
-        <div className='font-bold  pt-12'>@{username}</div>
-        <div className='pt-3 text-gray-400'>Lets help @{username} to get a Chai !</div>
-        <div className='pt-3 pb-5 text-gray-400'>56K members . 789 posts . $15,145/releases</div>
-    </div>
-    <div className='flex justify-center gap-3 pb-12 text-white'>
-        <div className='w-[40vw] max-h-[60vh] bg-slate-950 text-center overflow-y-auto scrollbar-hide px-10'>
-            <div className='font-bold my-2 py-5 text-center text-xl'>Top 10 Supporters</div>
-            <div className='text-gray-400 my-2 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1 min-h-8 min-w-8'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="user"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-            <div className='mb-8 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
-        </div>
-        <div className='w-[40vw] h-[60vh]  text-center bg-slate-950'>
-            <div className='font-bold p-5 my-2 text-xl'>Make a Payment</div>
-            <input type="text" placeholder='Enter name ' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleName} value={name}/>
-            <input type="text" placeholder='Enter message' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleMessage} value={message} />
-            <input type="text" placeholder='Enter amaount' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleInput} value={inputvalue} />
-            <button className='w-[30vw] h-10 bg-gradient-to-t from-[#7d3bef] from-0% to-[#6786ff] text-black my-2 rounded-3xl'>Pay</button>
-            <div className='flex mt-3 justify-center'>
-                <button className=' bg-slate-700 mr-2 rounded-3xl p-2' onClick={handlePay10}>Pay 10 Rs</button>
-                <button className=' bg-slate-700 mr-2 rounded-3xl p-2' onClick={handlePay20}>Pay 20 Rs</button>
-                <button className='bg-slate-700 rounded-3xl p-2' onClick={handlePay30}>Pay 30 Rs</button>
+    const handlePay10 = () => setInputValue(10);
+    const handlePay20 = () => setInputValue(20);
+    const handlePay30 = () => setInputValue(30);
+
+    const handlePay=async()=>{
+        const response = await fetch('/api/create-order', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ amount: inputvalue  })
+        });
+        const data = await response.json();
+        const { order_id } = data;
+
+        const options = {
+            key: process.env.KEY_ID,
+            amount: inputvalue,
+            currency: "INR",
+            name: name,
+            description: "Payment for " + username,
+            order_id: order_id,
+            handler: function (response) {
+                console.log(response);
+                alert("Payment Successful!");
+            },
+            prefill: {
+                name: name,
+                email: "example@gmail.com",
+                contact: "9876543210" 
+            },
+            notes: {
+                message: message || "No message"
+            },
+            theme: {
+                color: "#3399cc"
+            }
+        };
+        const rzp1 = new window.Razorpay(options);
+        rzp1.open();
+    }
+
+    return (
+        <div>
+            <div className='cover'>
+                <Image src="./landscape1.jpg" alt="image of backgroung" className=' h-72 w-[100%] object-cover ' />
             </div>
-            
+            <div className='h-1 bg-blue-950'></div>
+            <div className='text-white text-center relative'>
+                <div className='w-20 h-20 rounded-full bg-black border-2 overflow-hidden border-black mx-auto absolute left-[47vw] -top-11'>
+                    <Image src="./lion.jpeg" alt="image of lion" /></div>
+                <div className='font-bold  pt-12'>@{username}</div>
+                <div className='pt-3 text-gray-400'>Lets help @{username} to get a Chai !</div>
+                <div className='pt-3 pb-5 text-gray-400'>56K members . 789 posts . $15,145/releases</div>
+            </div>
+            <div className='flex justify-center gap-3 pb-12 text-white'>
+                <div className='w-[40vw] max-h-[60vh] bg-slate-950 text-center overflow-y-auto scrollbar-hide px-10'>
+                    <div className='font-bold my-2 py-5 text-center text-xl'>Top 10 Supporters</div>
+                    <div className='text-gray-400 my-2 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1 min-h-8 min-w-8'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="user"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-2 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                    <div className='mb-8 text-gray-400 flex'><div className='bg-white mr-3 border border-solid border-purple-950 rounded-full p-1'><Image src="user.svg" alt="profile pic"  ></Image></div><div><strong>{username}</strong> donated <strong>$</strong><strong>{username}</strong> with a message "<strong>{username}</strong>" . </div></div>
+                </div>
+                <div className='w-[40vw] h-[60vh]  text-center bg-slate-950'>
+                    <div className='font-bold p-5 my-2 text-xl'>Make a Payment</div>
+                    <input type="text" placeholder='Enter name ' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleName} value={name} />
+                    <input type="text" placeholder='Enter message' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleMessage} value={message} />
+                    <input type="text" placeholder='Enter amaount' className='w-[30vw] bg-slate-700 h-10 my-2 mx-auto p-4 rounded-3xl' onChange={handleInput} value={inputvalue} />
+                    <button className='w-[30vw] h-10 bg-gradient-to-t from-[#7d3bef] from-0% to-[#6786ff] text-black my-2 rounded-3xl' onClick={handlePay}>Pay</button>
+                    <div className='flex mt-3 justify-center'>
+                        <button className=' bg-slate-700 mr-2 rounded-3xl p-2' onClick={handlePay10}>Pay 10 Rs</button>
+                        <button className=' bg-slate-700 mr-2 rounded-3xl p-2' onClick={handlePay20}>Pay 20 Rs</button>
+                        <button className='bg-slate-700 rounded-3xl p-2' onClick={handlePay30}>Pay 30 Rs</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-  )
+    )
 }
 
 export default Username
